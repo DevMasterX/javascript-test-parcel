195d4e227873292ce1e57a6367e42546478f834b
DevMasterX <sokol080288@gmail.com>
2024-11-13 01:15:37 +0200

    46465

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/dist/basicLightbox.min.css';
 
+import { common } from './common';
+import { createMarkup } from './helpers/createMarkup';
+
 const instruments = [
   {
     id: 1,
@@ -67,9 +70,14 @@ const instruments = [
       'Бензиновий генератор GX-25 номінальною потужністю 2,5 кВт забезпечить автономність побутових приладів на дачі або у приватному будинку. Ви зможете одночасно підключити до нього освітлення, холодильник, зарядку телефону, ноутбук та водяний насос.',
   },
 ];
+// const KEY_FAVORITE = 'favorite';
+// const KEY_BASKET = 'basket';
 
 const search = document.querySelector('.js-search');
 const list = document.querySelector('.js-list');
+
+const favoriteArr = JSON.parse(localStorage.getItem(common.KEY_FAVORITE)) ?? [];
+const basketArr = JSON.parse(localStorage.getItem(common.KEY_BASKET)) ?? [];
 
 //  {
 //     id: 8,
@@ -79,27 +87,7 @@ const list = document.querySelector('.js-list');
 //     description:
 //       'Бензиновий генератор GX-25 номінальною потужністю 2,5 кВт забезпечить автономність побутових приладів на дачі або у приватному будинку. Ви зможете одночасно підключити до нього освітлення, холодильник, зарядку телефону, ноутбук та водяний насос.',
 //   },
-
-function createMarkup(arr) {
-  const markup = arr
-    .map(
-      ({ id, img, name }) => `
-  <li data-id="${id}" class='js-card'>
-      <img src="${img}" alt="${name}" width='300' />
-      <h2>${name}</h2>
-      <p ><a class='js-info' href='#'>More information</a></p>
-      <div>
-        <button>Add to favorite</button>
-        <button>Add to basket</button>
-      </div>
-    </li>
-  
-    `
-    )
-    .join('');
-  list.innerHTML = markup;
-}
-
+createMarkup(instruments, list);
 list.addEventListener('click', onClick);
 
 function onClick(e) {
@@ -114,18 +102,43 @@ function onClick(e) {
         <h3>${product.price}</h3>
         <p>${product.description}</p>
         <div>
-          <button>Add to favorite</button>
-          <button>Add to basket</button>
+          <button class='js-favorite'>Add to favorite</button>
+          <button class='js-basket'>Add to basket</button>
         </div>
       </div>
     </header>
 `);
     instance.show();
   }
-}
+  if (e.target.classList.contains('js-basket')) {
+    const { id } = e.target.closest('.js-card').dataset;
+    const product = findProduct(Number(id));
 
-createMarkup(instruments);
+    const inStorage = favoriteArr.some(({ id }) => id === product.id);
+    if (inStorage) {
+      return;
+    }
+
+    basketArr.push(product);
+    localStorage.setItem(common.KEY_BASKET, JSON.stringify(basketArr));
+  }
+
+  if (e.target.classList.contains('js-favorite')) {
+    const { id } = e.target.closest('.js-card').dataset;
+    const product = findProduct(Number(id));
+
+    const inStorage = favoriteArr.some(({ id }) => id === product.id);
+    if (inStorage) {
+      return;
+    }
+
+    favoriteArr.push(product);
+    localStorage.setItem(common.KEY_FAVORITE, JSON.stringify(favoriteArr));
+  }
+}
 
 function findProduct(productID) {
   return instruments.find(({ id }) => id === productID);
 }
+
+console.log(3);
