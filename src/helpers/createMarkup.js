714e5859944d04a879b20846a02b2186d18dc68b
DevMasterX <sokol080288@gmail.com>
2024-11-14 02:19:06 +0200

    985984

--- src/helpers/createMarkup.js
+++ src/helpers/createMarkup.js
@@ -1,7 +1,9 @@
 function createMarkup(arr, list) {
-  const markup = arr
-    .map(
-      ({ id, img, name }) => `
+  let markup;
+  if (arr.length) {
+    markup = arr
+      .map(
+        ({ id, img, name }) => `
   <li data-id="${id}" class='js-card'>
       <img src="${img}" alt="${name}" width='300' />
       <h2>${name}</h2>
@@ -11,10 +13,16 @@ function createMarkup(arr, list) {
           <button class='js-basket'>Add to basket</button>
       </div>
     </li>
-  
     `
-    )
-    .join('');
+      )
+      .join('');
+  } else {
+    markup = `
+  <li >
+      <img src="https://thumbs.dreamstime.com/b/empty-wicker-basket-black-white-isolated-white-background-basket-empty-wicker-basket-basket-vector-icon-black-white-302325782.jpg" alt="404" width='600' />
+    </li>
+    `;
+  }
   list.innerHTML = markup;
 }
 
