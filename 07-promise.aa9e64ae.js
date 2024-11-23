7881ba8233fa7ab4cb1caf69390d3dda4d3131bd
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2024-11-23 12:55:58 +0000

    Deploying to gh-pages from @ DevMasterX/javascript-test-parcel@486aec27aff66c24e1fb4b09a21ff496491d0841 🚀

---
+++ 07-promise.aa9e64ae.js
@@ -0,0 +1,2 @@
+var promise=new Promise((function(e,n){var o=Math.random()>.5;setTimeout((function(){o&&e("Промис выполнился успешно, с результатом")}),2e3)}));
+//# sourceMappingURL=07-promise.aa9e64ae.js.map
