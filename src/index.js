3f7a60c46520f81653bf2f73286a8da7fc0f6982
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2024-12-10 16:24:19 +0200

    http - practice

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
 import './css/common.css';
-// const logMessage = () => {
-//   console.log('Лог при вызове колбэк функции через 3 секунды');
-// };
-// console.log('до вызова setTimeout');
-
-// setTimeout(() => {
-//   console.log('внутри  callback для setTimeout');
-// }, 2000);
-
-// console.log('после вызова setTimeout');
-// console.log(Date.now());
-// console.log(Date.now());
