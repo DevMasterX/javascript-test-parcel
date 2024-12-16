ed8132edd6b496645829a6897536343a30a256aa
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2024-12-16 11:30:39 +0000

    Deploying to gh-pages from @ DevMasterX/javascript-test-parcel@f4cb17f4fdcb3eefab3e14954e369263e1858b68 🚀

--- pokemon.d9fe869e.js
+++ pokemon.d9fe869e.js
@@ -15,4 +15,4 @@ const e=document.querySelector(".js-card-container");function t(t){let u=functio
 </div>
     
     `}(t);e.innerHTML=u}function u(e){alert(`\u{1F62E}\u{1F62E}\u{423}\u{43F}\u{441}, \u{447}\u{442}\u{43E}-\u{442}\u{43E} \u{43F}\u{43E}\u{448}\u{43B}\u{43E} \u{43D}\u{435} \u{442}\u{430}\u{43A}!!!!!, Error: ${e}`)}document.querySelector(".js-search-form").addEventListener("submit",function(c){var a;c.preventDefault();let r=c.currentTarget,i=r.elements.query.value;e.innerHTML='<div class="loader"></div>',(a=i,fetch(`https://pokeapi.co/api/v2/pokemon/${a}`).then(e=>e.json())).then(t).catch(u).finally(()=>{r.reset(),function(){let t=e.querySelector(".loader");t&&t.remove()}()})}),fetch("https://pixabay.com/api/?key=39861298-38e1e10cd9c08fce6ef5c3957&q=yellow+flowers&image_type=photo").then(e=>e.json()).then(console.log);
-//# sourceMappingURL=08-HTTP.d94cf6b5.js.map
+//# sourceMappingURL=pokemon.d9fe869e.js.map
---
+++ weather.572e1aa0.js
@@ -0,0 +1,7 @@
+const e=document.querySelector(".js-search"),t=document.querySelector(".js-list");e.addEventListener("submit",function(e){var a,r;e.preventDefault();let{query:n,days:o}=e.currentTarget.elements;(a=n.value,r=o.value,fetch(`http://api.weatherapi.com/v1/forecast.json?key=1f583b8645b54c449e5214937241512&q=${a}&days=${r}&lang=uk`).then(e=>{if(!e.ok)throw Error(e.statusText);return e.json()})).then(e=>t.innerHTML=e.forecast.forecastday.map(({date:e,day:{avgtemp_c:t,condition:{icon:a,text:r}}})=>`<li>
+  <img src="${a}" alt="${r}">
+  <p>${r}</p>
+  <h2>${e}</h2>
+  <h3>Avg temp: ${t} C</h3>
+</li> `).join("")).catch(e=>console.log(e))});
+//# sourceMappingURL=weather.572e1aa0.js.map
