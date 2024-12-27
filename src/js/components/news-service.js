f8da74fd32314f9ba99cab1c70c898b9b297c941
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2024-12-27 15:02:00 +0200

    165465

--- src/js/components/news-service.js
+++ src/js/components/news-service.js
@@ -1,21 +1,34 @@
 export default class NewsApiService {
   constructor() {
     this.searchQuery = '';
+    this.page = 1;
   }
   fetchArticles() {
-    console.log(this);
     const options = {
       headers: {
         Authorization: '53234c7a30c3487e8bcad8ad4e056dba',
       },
     };
 
-    const url = `https://newsapi.org/v2/everything?q=${this.searchQuery}&language=en&pageSize=10&page=1`;
+    const url = `https://newsapi.org/v2/everything?q=${this.searchQuery}&language=en&pageSize=10&page=${this.page}`;
 
-    fetch(url, options)
+    return fetch(url, options)
       .then(r => r.json())
-      .then(console.log());
+      .then(data => {
+        this.incrementPage();
+
+        return data.articles;
+      });
   }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
   get query() {
     return this.query;
   }
--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,5 +1,7 @@
 import '../css/pagination.css';
 import NewsApiService from './components/news-service';
+import createArticlesMarkup from './helpers/createArticlesMarkup';
+console.log('🚀  createArticlesMarkup:', createArticlesMarkup);
 
 const refs = {
   searchForm: document.querySelector('.js-search-form'),
@@ -16,12 +18,21 @@ function onSearch(e) {
   e.preventDefault();
 
   newsApiService.query = e.currentTarget.elements.query.value;
+  newsApiService.resetPage();
 
-  newsApiService.fetchArticles(searchQuery);
+  newsApiService.fetchArticles().then(articles => console.log(articles));
+  appendArticlesMarkup(articles);
 }
 
 function onLoadMore() {
-  newsApiService.fetchArticles(searchQuery);
+  newsApiService.fetchArticles().then(articles => console.log(articles));
+}
+
+function appendArticlesMarkup(articles) {
+  refs.articlesContainer.insertAdjacentHTML(
+    'beforeend',
+    createArticlesMarkup(articles)
+  );
 }
 
 // const API_KEY = '53234c7a30c3487e8bcad8ad4e056dba';
