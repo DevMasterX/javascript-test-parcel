69dcf269821dd8a89ac638c4cbbf5e6f8fcedd1f
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2024-12-31 09:20:17 +0000

    Deploying to gh-pages from @ DevMasterX/javascript-test-parcel@efb6cdd71317186283021774805528b8f20d3268 🚀

---
+++ pagination-1.5031a90b.js
@@ -0,0 +1,2 @@
+document.querySelector(".js-guard"),new IntersectionObserver(function(e){console.log(evt)},{root:null,rootMargin:"200px",threshold:1});
+//# sourceMappingURL=pagination-1.5031a90b.js.map
---
+++ pagination-1.b8b0b7d2.js
@@ -0,0 +1,2 @@
+document.querySelector(".js-guard"),new IntersectionObserver(function(e){console.log(evt)},{root:null,rootMargin:"200px",threshold:1});
+//# sourceMappingURL=pagination-1.b8b0b7d2.js.map
