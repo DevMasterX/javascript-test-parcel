4d9f534f487ce5e36f7a88aa3772b6dcec82ee8e
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2025-01-10 14:17:59 +0000

    Deploying to gh-pages from @ DevMasterX/javascript-test-parcel@32ee51b5035cc7076d65847c9107775a94548ca1 🚀

---
+++ CRUD.05ec0c6c.js
@@ -0,0 +1,2 @@
+fetch("".concat("http://localhost:3000","/books/1"),options).then(function(o){return o.json()});
+//# sourceMappingURL=CRUD.05ec0c6c.js.map
---
+++ CRUD.9e1b56fc.js
@@ -0,0 +1,2 @@
+fetch("http://localhost:3000/books/1",options).then(o=>o.json());
+//# sourceMappingURL=CRUD.9e1b56fc.js.map
