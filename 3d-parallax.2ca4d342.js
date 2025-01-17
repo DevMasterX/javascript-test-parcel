3bb7de63d507049ae0976235d3dd743682329a02
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2025-01-17 12:58:39 +0000

    Deploying to gh-pages from @ DevMasterX/javascript-test-parcel@93c314aca4d7ebc73ea60d656c523edd3b6a72eb 🚀

---
+++ 3d-parallax.2ca4d342.js
@@ -0,0 +1,2 @@
+document.addEventListener("mousemove",function(n){Object.assign(document.documentElement,{style:"\n    --moove-x: ".concat(n.clientX,";\n    --move-y: ").concat(n.clientY,";\n    \n      ")})});
+//# sourceMappingURL=3d-parallax.2ca4d342.js.map
---
+++ 3d-parallax.9cbf40a8.js
@@ -0,0 +1,6 @@
+document.addEventListener("mousemove",e=>{Object.assign(document.documentElement,{style:`
+    --moove-x: ${e.clientX};
+    --move-y: ${e.clientY};
+    
+      `})});
+//# sourceMappingURL=3d-parallax.9cbf40a8.js.map
